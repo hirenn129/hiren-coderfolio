9398cfb96f26e8b9ac1e675522d97e442327452d
Hiren Chavda <hchavda4122001@gmail.com>
2024-01-12 16:09:10 +0530

    feed:Create portfolio

--- indexpage/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class IndexpageConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "indexpage"
--- indexpage/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-
-class User(models.Model):
-    discription = models.TextField(null=True, blank=True)
-    email = models.EmailField()
-    name = models.CharField(max_length=255)
-    subject = models.CharField(max_length=128)
